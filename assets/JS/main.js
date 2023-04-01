fba7c7bd5c0d548e12f164a27bb44da3940cb4eb
Juan Carlos Arriagada <109632032+jarriagadatorres@users.noreply.github.com>
2023-04-01 10:44:16 -0300

    cambios

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -17,23 +17,30 @@ async function LeerApiMonedas(crypto) {
 }
 
 
-async function LeerApiTop30(crypto) {
+async function LeerApiTop(crypto) {
 
-        const url = `https://min-api.cryptocompare.com/data/top/totaltoptiervolfull?tsym=USD&page=2`;
+        const url = `https://min-api.cryptocompare.com/data/top/totaltoptiervolfull?tsym=USD&page=1`;
         const datos = await fetch(url)
                 .then(response => response.json())
                 .then(data => {
                         return data;
                 });
-console.log(datos)
-        /*document.getElementById('crypto-usd').innerHTML = `
-        <h3>USD$ ${datos.USD}</h3>`;
-        document.getElementById('crypto-euro').innerHTML = `
-        <h3>EURO$ ${datos.EUR}</h3>`;
-        document.getElementById('crypto-clp').innerHTML = `
-        <h3>CLP$ ${datos.CLP}</h3>`*/
-        
+        console.table(datos)
+        document.getElementById('lista-tabla').innerHTML = `
+        <tr>
+                <th>logo</th>
+                <th>Moneda</th>
+                <th>Nombre</th>
+        </tr>`
+        for(i=0;i<5;i++){
+                document.getElementById('lista-tabla').innerHTML += `
+                <tr>
+                        <td>logo ${i}</td>
+                        <td>moneda ${i}</td>
+                        <td>Nombre ${i}</td>
+                </tr>`
+        }
 }
 
 LeerApiMonedas('BTC')
-LeerApiTop30()
+LeerApiTop()
