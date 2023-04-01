0ba678c7fbbce0a4436a6a5a199c37e1a69b4ae3
Juan Carlos Arriagada <109632032+jarriagadatorres@users.noreply.github.com>
2023-04-01 18:29:32 -0300

    Funciones JS y APIs

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -13,7 +13,7 @@ async function LeerApiMonedas(crypto) {
         <h3>EURO$ ${datos.EUR}</h3>`;
         document.getElementById('crypto-clp').innerHTML = `
         <h3>CLP$ ${datos.CLP}</h3>`
-        
+
 }
 
 
@@ -25,22 +25,38 @@ async function LeerApiTop(crypto) {
                 .then(data => {
                         return data;
                 });
-        console.table(datos)
-        document.getElementById('lista-tabla').innerHTML = `
-        <tr>
-                <th>logo</th>
-                <th>Moneda</th>
-                <th>Nombre</th>
-        </tr>`
-        for(i=0;i<5;i++){
+        document.getElementById('lista-tabla').innerHTM += '<tbody>';
+        for (let i = 0; i < 11; i++) {
                 document.getElementById('lista-tabla').innerHTML += `
                 <tr>
                         <td>logo ${i}</td>
                         <td>moneda ${i}</td>
-                        <td>Nombre ${i}</td>
+                        <td>nombre ${i}</td>
                 </tr>`
         }
+        document.getElementById('lista-tabla').innerHTM += `</tbody>`
 }
+function graficar(datos, tipo) {
+        switch (tipo) {
+                case 1:         //grafico de linea
+
+                case 2:         // grafico de area
+                case 3:         //grafico de bvarras
+        }
+};
+
+
+async function LeerApiGrafico(crypto) {
+        const url = `https://min-api.cryptocompare.com/data/top/totaltoptiervolfull?tsym=USD&page=1`;
+        const datos = await fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                        return data;
+                });
+
+        graficar(datos, typografico)
+}
+
 
 LeerApiMonedas('BTC')
 LeerApiTop()
